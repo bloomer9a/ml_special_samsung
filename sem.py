e655976dba4e6e88f9f1628ca7fe36dd4ecebdf0
bloomer9a <ajhgreg@gmail.com>
2022-05-30 07:06:01 +0000

    updated sem.py

--- sem.py
+++ sem.py
@@ -1,1 +1,52 @@
-import os 
+import os
+import glob
+import torch
+import torchvision.transforms as T
+from PIL import Image
+from torch.utils.data import Dataset, DataLoader
+
+
+class SEMDepthDataset(Dataset):
+    def __init__(self, data_path, transforms=False):
+        depth_path = os.path.join(data_path, 'Depth')
+        self.sem_path = os.path.join(data_path, 'SEM')
+        self.sem_list = [f for f in os.listdir(self.sem_path) if f.endswith('.png')]
+        self.depth_dict = self.get_depth_dict(depth_path)
+        self.transforms = transforms
+        if self.transforms:
+            self.augmentation = torch.nn.Sequential(
+                T.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+            )
+
+    def __getitem__(self, idx):
+        sem, depth = self.get_sem_and_depth(idx)
+        if self.transforms:
+            sem = self.augmentation(sem)
+        return sem, depth
+
+    def __len__(self):
+        return len(self.sem_list)
+
+    def get_depth_dict(self, depth_path):
+        depth_file_list = [f for f in os.listdir(depth_path) if f.endswith('.png')]
+        depth_file_dict = {}
+        for depth_file in depth_file_list:
+            file_name = depth_file.split('.png')[0]
+            depth_file_dict[file_name] = T.ToTensor()(Image.open(os.path.join(depth_path, depth_file)))
+
+        return depth_file_dict
+
+    def get_sem_and_depth(self, idx):
+        sem_file = self.sem_list[idx]
+        key = sem_file.split('_itr')[0]
+        sem = T.ToTensor()(Image.open(os.path.join(self.sem_path, sem_file)))
+        depth = self.depth_dict[key]
+        return sem, depth
+
+
+if __name__ == '__main__':
+    dataset = SEMDepthDataset(data_path='./data/Train')
+    loader = DataLoader(dataset)
+    loader_iter = iter(loader)
+    sem, depth = next(loader_iter)
+    print(f'sem:{sem.shape} depth:{depth.shape}')
