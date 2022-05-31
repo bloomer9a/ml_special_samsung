485930df0b232277618d3b4ba59729bac7de8d17
livviee <cloud09sm@gmail.com>
2022-05-31 05:10:05 +0000

    added random flip & commented all the changes

--- sem.py
+++ sem.py
@@ -4,6 +4,7 @@ import torch
 import torchvision.transforms as T
 from PIL import Image
 from torch.utils.data import Dataset, DataLoader
+import numpy as np
 
 
 class SEMDepthDataset(Dataset):
@@ -21,7 +22,8 @@ class SEMDepthDataset(Dataset):
     def __getitem__(self, idx):
         sem, depth = self.get_sem_and_depth(idx)
         if self.transforms:
-            sem = self.augmentation(sem)
+            sem, depth = self.random_flip(sem, depth) # added
+            #sem = self.augmentation(sem) # raises error
         return sem, depth
 
     def __len__(self):
@@ -43,28 +45,30 @@ class SEMDepthDataset(Dataset):
         depth = self.depth_dict[key]
         return sem, depth
 
-
-class RandomFlip(object):
-    def __call__(self, data):
-        label, input = data['label'], data['input']
+    
+    # added method
+    def random_flip(self, sem, depth):
+        h_flip = torch.nn.Sequential(
+            T.RandomHorizontalFlip(p=1)
+        )
+        v_flip = torch.nn.Sequential(
+            T.RandomVerticalFlip(p=1)
+        )
+        if np.random.rand() > 0.5:
+            sem = h_flip(sem)
+            depth = h_flip(depth)
 
         if np.random.rand() > 0.5:
-            label = np.fliplr(label)
-            input = np.fliplr(input)
+            sem = v_flip(sem)
+            depth = v_flip(depth)
 
-        if np.random.rand() > 0.5:
-            label = np.flipud(label)
-            input = np.flipud(input)
-
-        data = {'label': label, 'input': input}
-
-        return data
+        return sem, depth
 
 
 
 
 if __name__ == '__main__':
-    dataset = SEMDepthDataset(data_path='./data/Train')
+    dataset = SEMDepthDataset(data_path='./data/Train', transforms=True) # changed argument 
     loader = DataLoader(dataset)
     loader_iter = iter(loader)
     sem, depth = next(loader_iter)
